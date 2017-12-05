5a0b3346b9ea2ff9f73a9b236b2d5147dbf784cc
Luhan Lacerda <luhanlacerda@gmail.com>
2017-12-04 22:51:25 -0300

    Correção de erros na tela ListarConvenio, implementação do bean e tela de inserir Secretaria

--- src/beans/CrudConvenio.java
+++ src/beans/CrudConvenio.java
@@ -1,18 +1,27 @@
 package beans;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 import classesBasicas.Convenio;
 import negocios.Fachada;
 
 @ManagedBean
+@SessionScoped
 public class CrudConvenio {
 	private String id;
 	private String descricao;
-	
-	Fachada fachada = new Fachada();
-	Convenio convenio = new Convenio();
-	String mensagem;
+	private Fachada fachada;
+	private Convenio convenio;
+	private String mensagem;
+	private List<Convenio> convenios;
+
+	public CrudConvenio() {
+		this.fachada = new Fachada();
+		this.convenio = new Convenio();
+	}
 
 	public String getId() {
 		return id;
@@ -30,6 +39,14 @@ public class CrudConvenio {
 		this.descricao = descricao;
 	}
 
+	public Convenio getConvenio() {
+		return convenio;
+	}
+
+	public void setConvenio(Convenio convenio) {
+		this.convenio = convenio;
+	}
+
 	public String getMensagem() {
 		return mensagem;
 	}
@@ -38,14 +55,39 @@ public class CrudConvenio {
 		this.mensagem = mensagem;
 	}
 
-	public String cadastrarConvenio() {
+	public List<Convenio> getConvenios() throws Exception {
+		this.convenios = fachada.getAll(Convenio.class);
+		return this.convenios;
+	}
+
+	public void setConvenios(List<Convenio> convenios) {
+		this.convenios = convenios;
+	}
+
+	public String novo() {
+		convenio = new Convenio();
+		return "CadastrarConvenio";
+	}
+
+	public String editar(Convenio convenio) throws Exception {
+		fachada.update(convenio);
+		return "";
+	}
+
+	public String remover(Convenio convenio) throws Exception {
+		fachada.remove(convenio);
+		return "";
+	}
+
+	public String cadastrarConvenio() throws Exception {
 		convenio.setDescricao(descricao);
-		if (convenio.getDescricao().equals(null)) {
-			setMensagem("Favor informar a descrição");
-		} else {
+		if (!convenio.getDescricao().trim().isEmpty()) {
 			fachada.insert(convenio);
 			setMensagem("Convenio cadastrado com sucesso");
+		} else {
+			setMensagem("Favor informar a descrição");
 		}
 		return mensagem;
 	}
+
 }
